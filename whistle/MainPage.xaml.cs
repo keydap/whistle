e89f58ea0558433597e9b8c0d554ce2533f4fd97
kayyagari <kayyagari@apache.org>
2019-01-30 21:12:17 +0100

    app basics

---
+++ whistle/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace whistle
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"clicked = {count}";
+        }
+    }
+}
