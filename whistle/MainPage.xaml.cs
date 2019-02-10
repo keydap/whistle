bf4843a1d74fff274ef9bd0b05ba4d31a94b252b
kayyagari <kayyagari@apache.org>
2019-02-10 21:28:14 +0100

    o layout changes o added ZXing package

--- whistle/MainPage.xaml.cs
+++ whistle/MainPage.xaml.cs
@@ -13,12 +13,26 @@ namespace whistle
         public MainPage()
         {
             InitializeComponent();
+            StackLayout topLayout = (Content as StackLayout);
+            StackLayout otpLayout = (StackLayout)((ScrollView)topLayout.Children[1]).Content;
+            addTokens(otpLayout);
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
             count++;
             ((Button)sender).Text = $"clicked = {count}";
+        }
+
+        void addTokens(StackLayout otpLayout)
+        {
+            int count = 27;
+            for(int i=0; i < count; i++)
+            {
+                Label l = new Label();
+                l.Text = String.Format("{0:000000}", i);
+                otpLayout.Children.Add(l);
+            }
         }
     }
 }
